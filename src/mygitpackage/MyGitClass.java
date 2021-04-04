8459217c8a6296e303cd8f13348676957c7e8dc6
mesut <81835268+mmesutaksu@users.noreply.github.com>
2021-04-04 22:19:44 +0200

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -6,8 +6,6 @@ public class MyGitClass {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("My third commit");
-		System.out.println("Cigoladali Bazda yer misin Belgazz?");
-		System.out.println("Hozam Emre daha cok zever Bazda.");
 	}
 
 }
